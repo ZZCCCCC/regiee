756e2677693eacd21fbc2bf43cab308613477533
zzc <3210696396@qq.com>
2023-07-05 16:05:51 +0800

    添加缓存

--- src/main/java/com/zzc/reggie/controller/DishController.java
+++ src/main/java/com/zzc/reggie/controller/DishController.java
@@ -17,9 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
